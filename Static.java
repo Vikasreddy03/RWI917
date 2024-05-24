d1b6c095a8ada67d3cac76b675a67a0dcd212576
Vikasreddy03 <konkayalavikasreddy@gmail.com>
2024-05-24 06:35:53 +0530

    Static Block

---
+++ Static.java
@@ -0,0 +1,13 @@
+package StaticBlock;
+
+public class Static 
+{
+	static
+	 {
+		 System.out.println("Konkayala");
+	 }
+	 public static void main(String[]args)
+	 {
+		 System.out.println("Vikas Reddy");
+	 }
+}
